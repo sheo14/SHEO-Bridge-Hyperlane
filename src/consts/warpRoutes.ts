87ec32fa95b4cad599bfda6ff163b39cdb52e7e2
sheo <137234958+sheo14@users.noreply.github.com>
2024-10-07 20:03:16 +0200

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -4,6 +4,34 @@ import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [],
+  tokens: [
+    {
+      chainName: "arbitrum",
+      standard: "EvmHypCollateral",
+      decimals: 18,
+      symbol: "ARB",
+      name: "Arbitrum",
+      addressOrDenom: "0x632f432b125F189d1085B9e9d8dD010047E55101",
+      collateralAddressOrDenom: "0x912ce59144191c1204e64559fe8253a0e49e6548",
+      connections: [
+        {
+          token: "ethereum|base|0x632f432b125F189d1085B9e9d8dD010047E55101",
+        },
+      ],
+    },
+    {
+      chainName: "base",
+      standard: "EvmHypSynthetic",
+      decimals: 18,
+      symbol: "ARB",
+      name: "Base",
+      addressOrDenom: "0x632f432b125F189d1085B9e9d8dD010047E55101",
+      connections: [
+        {
+          token: "ethereum|arbitrum|0x632f432b125F189d1085B9e9d8dD010047E55101",
+        },
+      ],
+    },
+  ],
   options: {},
 };
