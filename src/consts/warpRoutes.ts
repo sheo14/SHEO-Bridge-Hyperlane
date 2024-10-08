490bbf07d650e664127dfe535dfd7ff37cf7c483
sheo <137234958+sheo14@users.noreply.github.com>
2024-10-08 14:38:00 +0200

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -4,29 +4,29 @@ import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
 export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
-      chainName: "arbitrum",
-      standard: "EvmCollateral" as any, // Forcer le typage si nécessaire
+      chainName: 'arbitrum',
+      standard: 'EvmCollateral',
       decimals: 18,
-      symbol: "ARB",
-      name: "Arbitrum",
-      addressOrDenom: "0x632f432b125F189d1085B9e9d8dD010047E55101",
-      collateralAddressOrDenom: "0x912ce59144191c1204e64559fe8253a0e49e6548",
+      symbol: 'ARB',
+      name: 'Arbitrum',
+      addressOrDenom: '0x632f432b125F189d1085B9e9d8dD010047E55101',
+      collateralAddressOrDenom: '0x912ce59144191c1204e64559fe8253a0e49e6548',
       connections: [
         {
-          token: "ethereum|base|0x632f432b125F189d1085B9e9d8dD010047E55101",
+          token: 'ethereum|base|0x632f432b125F189d1085B9e9d8dD010047E55101',
         },
       ],
     },
     {
-      chainName: "base",
-      standard: "EvmHypSynthetic", // Cette valeur semble déjà correcte
+      chainName: 'base',
+      standard: 'EvmHypSynthetic',
       decimals: 18,
-      symbol: "ARB",
-      name: "Base",
-      addressOrDenom: "0x632f432b125F189d1085B9e9d8dD010047E55101",
+      symbol: 'ARB',
+      name: 'Base',
+      addressOrDenom: '0x632f432b125F189d1085B9e9d8dD010047E55101',
       connections: [
         {
-          token: "ethereum|arbitrum|0x632f432b125F189d1085B9e9d8dD010047E55101",
+          token: 'ethereum|arbitrum|0x632f432b125F189d1085B9e9d8dD010047E55101',
         },
       ],
     },
