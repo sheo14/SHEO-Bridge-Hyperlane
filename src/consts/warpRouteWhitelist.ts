e17932771f357cc1ffe6a63cd15c073f6921f50a
sheo <137234958+sheo14@users.noreply.github.com>
2024-10-09 14:33:33 +0200

    Update warpRouteWhitelist.ts

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -2,6 +2,8 @@
 // Warp Route IDs use format `SYMBOL/chainname1-chainname2...` where chains are ordered alphabetically
 // If left null, all warp routes in the configured registry will be included
 // If set to a list (including an empty list), only the specified routes will be included
-export const warpRouteWhitelist: Array<string> | null = [
-  "ARB/arbitrum-base",           // Route from Arbitrum to Base 
-];
+export const warpRouteWhitelist: Array<string> | null = null;
+// Example:
+// [
+//   // 'ETH/ethereum-viction'
+// ];
