8e9b50f3b2e6e9696e4b2808a96c4c3294551976
sheo <137234958+sheo14@users.noreply.github.com>
2024-10-09 13:57:14 +0200

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,36 +1,9 @@
 import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
 
 // A list of Warp Route token configs
+// These configs will be merged with the warp routes in the configured registry
+// The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-
-{
-  tokens: [
-    {
-      // The ChainName of the token
-      chainName: 'arbitrum',
-      // See https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/token/TokenStandard.ts
-      standard: TokenStandard.EvmHypCollateral,
-      // The token metadata (decimals, symbol, name)
-      decimals: 18,
-      symbol: 'ARB',
-      name: 'Arbitrum',
-      // The router address
-      addressOrDenom: '0x632f432b125F189d1085B9e9d8dD010047E55101',
-      // The address of the underlying collateral token
-      collateralAddress: '0x912ce59144191c1204e64559fe8253a0e49e6548',
-      // A path to a token logo image
-      logoURI: '/logos/usdc.png',
-      // The list of tokens this one is connected to
-      connections: [ { token: 'ethereum|Arbitrum|0x632f432b125F189d1085B9e9d8dD010047E55101' } ]
-    },
-    {
-      chainName: 'base',
-      standard: TokenStandard.EvmHypSynthetic,
-      decimals: 18,
-      symbol: 'ARB',
-      name: 'Arbitrum',0x632f432b125F189d1085B9e9d8dD010047E55101',
-      logoURI: '/logos/usdc.png',
-      connections: [ { token: 'ethereum|Arbitrum|0x632f432b125F189d1085B9e9d8dD010047E55101' } ]
-    }
-  ]
-}
+  tokens: [],
+  options: {},
+};
